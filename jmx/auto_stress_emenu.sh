72d3ade8a67e5d3b2d428b7956ee37b695ed35d2
Ji Zhi Qian <jizhi.qian@jimubox.com>
2020-05-30 17:56:53 +0800

    auto stress

---
+++ jmx/auto_stress_emenu.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# 压测脚本模板中设定的压测时间应为60秒
+export jmx_template="emenu"
+export suffix=".jmx"
+export jmx_template_filename="${jmx_template}${suffix}"
+export os_type=`uname`
+
+# 需要在系统变量中定义jmeter根目录的位置，如下
+# export jmeter_path="/your jmeter path/"
+
+# 清空nohup.out
+cat /dev/null > nohup.out
+
+# 强制杀掉JMeter进程
+killJMeter()
+{
+    pid=`ps -ef|grep jmeter|grep java|grep ${jmx_filename}|awk '{print $2}'`
+    echo "jmeter Id list :$pid"
+    if [[ "$pid" = "" ]]
+    then
+      echo "no jmeter pid alive"
+    else
+      kill -9 $pid
+    fi
+}
+echo "美餐网自动化压测全部开始"
+# 压测并发数列表
+thread_number_array=(10 20 30 40 50 100 200 300 400)
+for num in "${thread_number_array[@]}"
+do
+    # 生成对应压测线程的jmx文件
+    export jmx_filename="${jmx_template}_${num}${suffix}"
+    export jtl_filename="test_${num}.jtl"
+
+    rm -f ${jmx_filename} ${jtl_filename}
+    cp ${jmx_template_filename} ${jmx_filename}
+    echo "生成jmx压测脚本 ${jmx_filename}"
+
+    if [[ "${os_type}" == "Darwin" ]]; then
+        sed -i "" "s/thread_num/${num}/g" ${jmx_filename}
+    else
+        sed -i "s/thread_num/${num}/g" ${jmx_filename}
+    fi
+
+    # JMeter 静默压测
+    nohup ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename} &
+    sleep 65
+    killJMeter
+    rm -f ${jmx_filename}
+done
+echo "美餐网自动化压测全部结束"
+
